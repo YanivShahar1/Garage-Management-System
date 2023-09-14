e7467cca3e8a2531c9b072534a43d20b136742f8
YanivShahar <yanivshahar97@gmail.com>
2023-09-14 20:22:18 +0300

    Added PhoneNumber class and Info folder

--- GarageLogic/Info/OwnerInfo.cs
+++ GarageLogic/Info/OwnerInfo.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace GarageLogic.Vehicles
+namespace GarageLogic.Info
 {
     public struct OwnerInfo
     {
         public string OwnerName { get; }
-        public string OwnerPhone { get; }
-        public OwnerInfo(string ownerName, string ownerPhone)
+        public PhoneNumber OwnerPhone { get; }
+        public OwnerInfo(string ownerName, PhoneNumber ownerPhone)
         {
             OwnerName = ownerName ?? throw new ArgumentNullException(nameof(ownerName), "Owner name cannot be null.");
             OwnerPhone = ownerPhone ?? throw new ArgumentNullException(nameof(ownerPhone), "Owner phone cannot be null.");
