86ffcba7dddcab56344dc49762f4dd509b726945
YanivShahar <yanivshahar97@gmail.com>
2023-09-14 20:43:16 +0300

    feat: Automatically Determine Country in PhoneNumber Class
    Updated the PhoneNumber class to automatically determine the user's country based on their current culture settings. This enhancement allows the PhoneNumber class to adapt to the user's localization without the need to explicitly specify the country code when creating PhoneNumber objects. The class now includes properties for both the country code and the localized country name.
    
    This change improves the user experience and ensures that phone numbers are validated and stored with the correct country context.

--- GarageLogic/Info/OwnerInfo.cs
+++ GarageLogic/Info/OwnerInfo.cs
@@ -1,18 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Globalization;
 
 namespace GarageLogic.Info
 {
-    public struct OwnerInfo
+    public class OwnerInfo
     {
         public string OwnerName { get; }
-        public PhoneNumber OwnerPhone { get; }
-        public OwnerInfo(string ownerName, PhoneNumber ownerPhone)
+        public PhoneNumber OwnerPhoneNumber { get; }
+        public OwnerInfo(string ownerName, string ownerPhoneNumber)
         {
             OwnerName = ownerName ?? throw new ArgumentNullException(nameof(ownerName), "Owner name cannot be null.");
-            OwnerPhone = ownerPhone ?? throw new ArgumentNullException(nameof(ownerPhone), "Owner phone cannot be null.");
+            OwnerPhoneNumber = new PhoneNumber(ownerPhoneNumber);
+        }
+
+        private string GetCurrentCountryCode()
+        {
+            try
+            {
+                // Get the current region (country) code based on the current culture.
+                var currentRegion = RegionInfo.CurrentRegion;
+                return currentRegion.TwoLetterISORegionName;
+            }
+            catch (Exception)
+            {
+                // Handle cases where the country code cannot be determined.
+                return "Unknown";
+            }
+        }
+
+        private string GetCountryNameByCode(string code)
+        {
+            try
+            {
+                var regionInfo = new RegionInfo(code);
+                return regionInfo.DisplayName; // Get the localized country name.
+            }
+            catch (ArgumentException)
+            {
+                // Handle invalid country codes.
+                return "Unknown";
+            }
         }
     }
 }
--- GarageLogic/Info/PhoneNumber.cs
+++ GarageLogic/Info/PhoneNumber.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text.RegularExpressions;
 
 namespace GarageLogic.Info
@@ -7,13 +8,24 @@ namespace GarageLogic.Info
     public class PhoneNumber
     {
         private string _phoneNumber;
+        public string CountryCode { get; } 
+        public string CountryName { get; }
 
+        // Define a dictionary to store regular expression patterns for different countries.
+        private static Dictionary<string, string> CountryPatterns = new Dictionary<string, string>
+        {
+            { "US", @"^\d{10}$" }, // Example pattern for the United States (10-digit numbers).
+            { "UK", @"^\d{11}$" }, // Example pattern for the United Kingdom (11-digit numbers).
+            { "IL", @"^0[23489]{1}[0-9]{7}$" }, // Pattern for Israel (10-digit numbers starting with 0).
+            { "FR", @"^0[1-9]{1}[0-9]{8}$" }, // Pattern for France (10-digit numbers starting with 0).
+            // TODO : Add patterns for other countries as needed.
+        };
         public string Value
         {
             get { return _phoneNumber; }
             set
             {
-                if (IsValidPhoneNumber(value))
+                if (IsValidPhoneNumber(value, CountryCode))
                 {
                     _phoneNumber = value;
                 }
@@ -24,21 +36,52 @@ namespace GarageLogic.Info
             }
         }
 
-        public PhoneNumber(string phoneNumber)
+        public PhoneNumber(string ownerPhoneNumber, string countryCode)
         {
-            Value = phoneNumber;
+            Value = ownerPhoneNumber;
+            CountryCode = countryCode ?? throw new ArgumentNullException(nameof(countryCode), "Country code cannot be null.");
+            CountryName = GetCountryNameByCode(countryCode);
+        }
+        public PhoneNumber(string ownerPhoneNumber)
+        {
+            Value = ownerPhoneNumber;
+
+            // Automatically determine the user's country code based on current culture settings.
+            CountryCode = GetCurrentCountryCode();
+
+            // Get the localized country name based on the determined country code.
+            CountryName = GetCountryNameByCode(CountryCode);
         }
 
-        // Define a dictionary to store regular expression patterns for different countries.
-        private static Dictionary<string, string> CountryPatterns = new Dictionary<string, string>
+        private string GetCurrentCountryCode()
         {
-            { "US", @"^\d{10}$" }, // Example pattern for the United States (10-digit numbers).
-            { "UK", @"^\d{11}$" }, // Example pattern for the United Kingdom (11-digit numbers).
-            { "IL", @"^0[23489]{1}[0-9]{7}$" }, // Pattern for Israel (10-digit numbers starting with 0).
-            { "FR", @"^0[1-9]{1}[0-9]{8}$" }, // Pattern for France (10-digit numbers starting with 0).
-            // TODO - Add patterns for other countries as needed.
-        };
-        
+            try
+            {
+                // Get the current region (country) code based on the current culture.
+                var currentRegion = RegionInfo.CurrentRegion;
+                return currentRegion.TwoLetterISORegionName;
+            }
+            catch (Exception)
+            {
+                // Handle cases where the country code cannot be determined.
+                return "Unknown";
+            }
+        }
+
+        private string GetCountryNameByCode(string code)
+        {
+            try
+            {
+                var regionInfo = new RegionInfo(code);
+                return regionInfo.DisplayName; // Get the localized country name.
+            }
+            catch (ArgumentException)
+            {
+                // Handle invalid country codes.
+                return "Unknown";
+            }
+        }
+
         public static bool IsValidPhoneNumber(string phoneNumber, string country)
         {
             // Check if the specified country has a custom pattern defined.
